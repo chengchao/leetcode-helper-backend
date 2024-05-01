d81794253fba5eb73a8a14b74f0bc54e2db926cb
chengchao <chengchao60827@gmail.com>
2024-05-01 14:14:09 -0700

    feat: add prisma

---
+++ prisma/migrations/20240501210312_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "Difficulty" AS ENUM ('EASY', 'MEDIUM', 'HARD');
+
+-- CreateTable
+CREATE TABLE "Problem" (
+    "id" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "problemNumber" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "difficulty" "Difficulty" NOT NULL,
+    "tags" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Problem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Problem_slug_key" ON "Problem"("slug");
